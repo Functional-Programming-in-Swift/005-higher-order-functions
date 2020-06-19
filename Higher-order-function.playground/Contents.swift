fee4c8ff2a01d08edf2e080c12c4d5889961e583
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-19 19:16:11 -0400

    Make three functions to show message

--- Higher-order-function.playground/Contents.swift
+++ Higher-order-function.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
+// (Strinfg) -> Void
+func logToConsole(_ msg: String) {
+    print(msg + "From Console")
+}
 
+func logToFile(_ msg: String) {
+    print(msg + "From File")
+}
+
+func logToWindow(_ msg: String) {
+    print(msg + "From Windows")
+}
+
+logToConsole("Hello Swift FP ")
