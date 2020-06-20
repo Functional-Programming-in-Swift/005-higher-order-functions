cae4a894c3d18d7768e11d954149ce953a38fa03
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-19 22:21:58 -0400

    This is the solucion using concep function higher order function

--- Higher-order-function.playground/Contents.swift
+++ Higher-order-function.playground/Contents.swift
@@ -2,45 +2,23 @@ import UIKit
 
 // (Strinfg) -> Void
 func logToConsole(_ msg: String) {
-    print(msg + "From Console")
+    print(msg + " From Console")
 }
 
 func logToFile(_ msg: String) {
-    print(msg + "From File")
+    print(msg + " From File")
 }
 
 func logToWindow(_ msg: String) {
-    print(msg + "From Windows")
+    print(msg + " From Windows")
 }
 
-protocol Logger {
-    func print(_ msg: String)
-}
-
-class ConsoleLogger : Logger {
-    func print(_ msg: String) {
-        logToConsole(msg)
-    }
-}
-
-class FileLogger : Logger {
-    func print(_ msg: String) {
-        logToFile(msg)
-    }
-}
-
-class WindowsLogger : Logger {
-    func print(_ msg: String) {
-        logToWindow(msg)
-    }
-}
-
-func getValue(from number: Int, loggerType: Logger) -> String {
+func getValue(from number: Int, loggerType: (String)->()) -> String {
     
     let result = "something \(number)"
     
-    loggerType.print(result)
+    loggerType(result)
     return result
 }
 
-getValue(from: 5, loggerType: ConsoleLogger())
+getValue(from: 5, loggerType: logToWindow)
