7137400364c0cf7a589234cbf652f65f4e79ae0c
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-19 22:18:34 -0400

    Refactor this using object, but that is not idea in functional programming

--- Higher-order-function.playground/Contents.swift
+++ Higher-order-function.playground/Contents.swift
@@ -13,21 +13,34 @@ func logToWindow(_ msg: String) {
     print(msg + "From Windows")
 }
 
-func getValue(from number: Int, loggerType: Int) -> String {
+protocol Logger {
+    func print(_ msg: String)
+}
+
+class ConsoleLogger : Logger {
+    func print(_ msg: String) {
+        logToConsole(msg)
+    }
+}
+
+class FileLogger : Logger {
+    func print(_ msg: String) {
+        logToFile(msg)
+    }
+}
+
+class WindowsLogger : Logger {
+    func print(_ msg: String) {
+        logToWindow(msg)
+    }
+}
+
+func getValue(from number: Int, loggerType: Logger) -> String {
     
     let result = "something \(number)"
     
-    switch loggerType {
-    case 1:
-        logToConsole(result)
-    case 2:
-        logToWindow(result)
-    case 3:
-        logToFile(result)
-    default:
-        break
-    }
+    loggerType.print(result)
     return result
 }
 
-getValue(from: 5, loggerType: 1)
+getValue(from: 5, loggerType: ConsoleLogger())
